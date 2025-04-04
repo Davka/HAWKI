2eb75319c3321a2647f09a4a8ab7de14ad814aaf
David Siegfried <david.siegfried@uni-vechta.de>
2025-04-04 13:58:51 +0200

    refine routes

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::middleware('prevent_back')->group(function () {
     Route::get('/login', [LoginController::class, 'index']);
     Route::post('/req/login-ldap', [AuthenticationController::class, 'ldapLogin']);
     Route::post('/req/login-shibboleth', [AuthenticationController::class, 'shibbolethLogin']);
-    Route::post('/req/login-oidc', [AuthenticationController::class, 'openIDLogin']);
-    Route::get('/req/start-oidc', [AuthenticationController::class, 'startOpenIDLogin']);
+    Route::get('/req/login-oidc', [AuthenticationController::class, 'openIDLogin']);
+    Route::post('/req/start-oidc', [AuthenticationController::class, 'startOpenIDLogin']);
 
 
     Route::post('/req/changeLanguage', [LanguageController::class, 'changeLanguage']);
